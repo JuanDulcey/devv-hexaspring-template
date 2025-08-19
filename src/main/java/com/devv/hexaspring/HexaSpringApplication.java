570517437775507e64d7fe9ee503faff8cbb7368
JuanDulcey <juanedulcey05@gmail.com>
2025-08-19 17:08:26 -0500

    Initial commit: HexaSpring Template

---
+++ src/main/java/com/devv/hexaspring/HexaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devv.hexaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devv/hexaspring/HexaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devv.hexaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
